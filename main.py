2044670ba29cb3239eb7f6568d91f26701de9291
P4CK3TZ - OSIG <109151664+P4CK3TZ@users.noreply.github.com>
2023-01-21 02:21:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
+import base64
 from MaltegoTransform import *
 import requests
 
-class SpiderFootTransform(MaltegoTransform):
+# Encode the code
+encoded_code = base64.b64encode(b'''
+class a(MaltegoTransform):
     def __init__(self):
         MaltegoTransform.__init__(self)
 
-    def do_transform(self, request, response, config):
-        # Replace with your SpiderFoot API key
+    def b(self, request, response, config):
         api_key = "YOUR_API_KEY"
-        
-        # Get the item to send to SpiderFoot
-        item = request.Value
-        
-        # Set the base URL for the SpiderFoot API
-        base_url = "https://api.spiderfoot.net"
-        
-        # Build the URL for the scan API endpoint
-        url = f"{base_url}/scan?key={api_key}&target={item}"
-        
-        # Send the request to SpiderFoot
-        response = requests.get(url)
-        
-        # Check the status code of the response
-        if response.status_code == 200:
+        c = request.Value
+        d = "https://api.spiderfoot.net"
+        e = d + "/scan?key=" + api_key + "&target=" + c
+        f = requests.get(e)
+        if f.status_code == 200:
             response.addUIMessage("Item sent to SpiderFoot successfully!")
         else:
             response.addUIMessage("An error occurred while sending the item to SpiderFoot.")
 
 if __name__ == '__main__':
-    SpiderFootTransform()
+    a()
+''')
+
+# Decode the code
+decoded_code = base64.b64decode(encoded_code).decode()
+
+# Run the decoded code
+exec(decoded_code)
